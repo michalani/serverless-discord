0dd0ecaa98d74fcb24dfa8fe9d0cf518079507da
michalani <michal.anisimow@gmail.com>
2023-03-13 23:35:00 +0000

    v4

--- register.py
+++ register.py
@@ -1,15 +1,26 @@
 import requests
 
-with open('.env') as env_file:
-    for line in env_file:
-        if(line.split('=')[0] == "DISCORD_TOKEN"):
-            DISCORD_TOKEN = line.split('=')[1].splitlines()[0]
-        elif(line.split('=')[0] == "DISCORD_APPLICATION_ID"):
-            DISCORD_APPLICATION_ID = line.split('=')[1].splitlines()[0]
-        elif(line.split('=')[0] == "REGISTER_COMMAND"):
-            REGISTER_COMMAND = line.split('=')[1].splitlines()[0]
-        # print(),  # The comma to suppress the extra new line char
+# parse env file
+try:
+    with open('.env') as env_file:
+        for line in env_file:
+            if(line.split('=')[0] == "DISCORD_TOKEN"):
+                DISCORD_TOKEN = line.split('=')[1].splitlines()[0]
+            elif(line.split('=')[0] == "DISCORD_APPLICATION_ID"):
+                DISCORD_APPLICATION_ID = line.split('=')[1].splitlines()[0]
+            elif(line.split('=')[0] == "REGISTER_COMMAND"):
+                REGISTER_COMMAND = line.split('=')[1].splitlines()[0]
+            # Ignore, this is another method of authentication
+            elif(line.split('=')[0] == "DISCORD_CREDENTIALS_TOKEN"):
+                DISCORD_CREDENTIALS_TOKEN = line.split('=')[1].splitlines()[0]
 
+            # print(),  # The comma to suppress the extra new line char
+except Exception as e:
+    print(e)
+    print('Could not parse env file!')
+    quit()
+
+# Main logic for registering discord slash command directly with discord
 if(REGISTER_COMMAND != 'false'):
     url = "https://discord.com/api/v10/applications/{}/commands".format(DISCORD_APPLICATION_ID)
 
@@ -47,23 +58,25 @@ if(REGISTER_COMMAND != 'false'):
             }
         ]
     }
-
-    # For authorization, you can use either your bot token
-    headers = {
-        "Authorization": "Bot {}".format(DISCORD_TOKEN)
-    }
-
-    # # or a client credentials token for your app with the applications.commands.update scope
-    # headers = {
-    #     "Authorization": "Bearer <my_credentials_token>"
-    # }
+    try:
+        # For authorization, you can use either your bot token
+        headers = {
+            "Authorization": "Bot {}".format(DISCORD_TOKEN)
+        }
+    except NameError:
+        print('wrong name')
+        # or a client credentials token for your app with the applications.commands.update scope
+        headers = {
+            "Authorization": "Bearer {}".format(DISCORD_CREDENTIALS_TOKEN)
+        }
 
     r = requests.post(url, headers=headers, json=json)
 
     if(r.status_code == 200):
-        print('Successfully registered discord slash commands')
+        print('Successfully registered GLOBAL discord slash commands')
     else:
-        print('Discord slash command could not be registered')
+        print('Discord GLOBAL slash commands could not be registered')
         print(r.content)
+        # quit()
 else:
     print('Skipped registering discord slash command')
